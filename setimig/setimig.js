c6cfb878360572e28f00a23b0f9490f10f8d847c
Santi Miralda <smiralda@fiac.cat>
2020-03-22 19:26:56 +0100

    Versió 2 He creat algunes funcions i he utilitzat la funció forEach en comptes del for en un parell de cassos.

--- setimig/setimig.js
+++ setimig/setimig.js
@@ -7,29 +7,6 @@ const prompt = require("prompt-sync")({ sigint: true });
     Crearem una baralla barrejada que tindrà les cartes del 1 al 7 i les cartes del 10 al 12 amb el valor de 0.5
 */
 
-// Definim i creem la baralla espanyola com un array de cartes.
-// Cada carta és un objecte amb 4 camps: el pal, la preposició, el número i el valor en el set i mig.
-// El valor 0 indica que la carta no s'utilitza.
-
-const palsBarallaEsp = ["Oros", "Copes", "Espases", "Bastos"];
-const preposPals = ["d'", "de ", "d'", "de "];
-
-const numsBarallaEsp = [
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "11",
-  "12"
-];
-const valorsSIM = [1, 2, 3, 4, 5, 6, 7, 0, 0, 0.5, 0.5, 0.5];
-
 const nomBanca = "Banca";
 const saldoBanca = 500;
 const maxSaldoInicial = 50;
@@ -52,25 +29,34 @@ const ordinalRondes = [
 const maxJugadors = 5;
 const ordinalJugadors = [null, "Primer", "Segon", "Tercer", "Quart", "Cinquè"];
 
-//Creen la baralla espanyola sencera.
-const barallaEsp = [];
-let cartaEsp;
-for (let p = 0; p < 4; p++) {
-  for (let n = 0; n < 12; n++) {
-    cartaEsp = {
-      palCarta: palsBarallaEsp[p],
-      preposPal: preposPals[p],
-      numCarta: numsBarallaEsp[n],
-      valorSIM: valorsSIM[n]
-    };
-    barallaEsp.push(cartaEsp);
+// Funció que retorna una baralla espanyola ordenada.
+// Per carta tenim el valor de la carta en el set i mig.
+function obteBarallaEsp() {
+  const pals = ["Oros", "Copes", "Espases", "Bastos"];
+  const preposPals = ["d'", "de ", "d'", "de "];
+  const valorsSIM = [1, 2, 3, 4, 5, 6, 7, 0, 0, 0.5, 0.5, 0.5];
+
+  const barallaEsp = [];
+  for (let p = 0; p < 4; p++) {
+    for (let n = 0; n < 12; n++) {
+      barallaEsp.push({
+        numCarta: String(n+1),
+        palCarta: pals[p],
+        preposPal: preposPals[p],
+        valorSIM: valorsSIM[n]
+      });
+    }
   }
+  return barallaEsp;
 }
 
-// Obtenim una baralla amb només les cartes del set i mig i barregem la baralla.
-const barallaSIM = barrejaBaralla(
-  barallaEsp.filter(carta => carta.valorSIM != 0)
-);
+// Funció que obté una baralla amb només les cartes del set i mig i amb el valor de les cartes.
+function obteBarallaSIM(baralla) {
+  const barallaSIM = barrejaBaralla(
+    baralla.filter(carta => carta.valorSIM != 0)
+  );
+  return barallaSIM;
+}
 
 // Funció que barreja una baralla amb el mètode 'Fisher-Yates'
 function barrejaBaralla(baralla) {
@@ -133,6 +119,7 @@ function obteJugadors(numJugadors) {
 }
 
 let jugadors = obteJugadors(numJugadors);
+let banca = jugadors[0];
 
 //Es pregunta el nombre de rondes.
 let numRondes = obteValor(`Quantes rondes vols jugar`, maxRondes);
@@ -140,109 +127,112 @@ let numRondes = obteValor(`Quantes rondes vols jugar`, maxRondes);
 // S'inicia el joc.
 console.log(`Comença el joc.`);
 
+let baralla = obteBarallaSIM(obteBarallaEsp());
+
 // Es juga cada una de les rondes.
 for (let r = 1; r <= numRondes; r++) {
   console.log(`\n*** ${ordinalRondes[r]} ronda ***`);
-
+  
   //Es juga per cada un dels jugadors.
-  for (let i = 1; i <= jugadors.length - 1; i++) {
+  //Es comença per la posició 1 ja que la 0 és la banca.
+  for (let i = 1; i < jugadors.length; i++) {
     //Primer juga el jugador
-    console.log(`\n${jugadors[i].nom}, et toca a tu.`);
+    let jugador = jugadors[i];
+    if (jugador.saldo > 0) {
+      console.log(`\n${jugador.nom}, et toca a tu.`);
 
-    if (jugadors[i].saldo == 0) {
-      console.log("No tens saldo. No pots seguir jugant.");
-      break;
-    }
+      let aposta = 0;
+      let esCanviAposta = false;
+      let carta = {};
+      let esSimSuperat = false;
+      let esSimIgualat = false;
+      let guanyaLaBanca = false;
+      jugador.punts = 0;
 
-    let aposta = 0;
-    let esCanviAposta = false;
-    let carta = {};
-    let esSimSuperat = false;
-    let esSimIgualat = false;
-    let guanyaLaBanca = false;
-    let puntsJugador = jugadors[i].punts;
-
-    //Treu cartes fins que el jugador es planta o bé iguala o supera el set i mig.
-    do {
-      esCanviAposta = false;
-      carta = barallaSIM.shift();
-      puntsJugador += carta.valorSIM;
-      console.log(
-        `  ${carta.numCarta} ${carta.preposPal}${carta.palCarta}. Punts: ${puntsJugador}. Aposta: ${aposta}`
+      //Treu cartes fins que el jugador es planta o bé iguala o supera el set i mig.
+      do {
+        esCanviAposta = false;
+        carta = baralla.shift();
+        jugador.punts += carta.valorSIM;
+        console.log(
+          `  ${carta.numCarta} ${carta.preposPal}${carta.palCarta}. Punts: ${jugador.punts}. Aposta: ${aposta}`
+        );
+        if (jugador.punts > 7.5) {
+          esSimSuperat = true;
+        } else if (jugador.punts == 7.5) {
+          esSimIgualat = true;
+        } else {
+          if (
+            aposta == 0 ||
+            prompt(`Vols canviar la teva aposta (Si:s No:Intro)? `) == "s"
+          ) {
+            esCanviAposta = aposta != 0;
+            aposta = Number(
+              obteValor("Quina és la teva aposta?", jugador.saldo)
+            );
+          }
+        }
+      } while (
+        esCanviAposta ||
+        (!esSimSuperat &&
+          !esSimIgualat &&
+          prompt(`Et vols plantar (Si:s No:Intro)? `) != "s")
       );
-      if (puntsJugador > 7.5) {
-        esSimSuperat = true;
-      } else if (puntsJugador == 7.5) {
-        esSimIgualat = true;
+      if (esSimSuperat) {
+        console.log(`Has superat el límit!`);
+        guanyaLaBanca = true;
       } else {
-        if (
-          aposta == 0 ||
-          prompt(`Vols canviar la teva aposta (Si:s No:Intro)? `) == "s"
-        ) {
-          esCanviAposta = aposta != 0;
-          aposta = Number(
-            obteValor("Quina és la teva aposta?", jugadors[i].saldo)
+        if (esSimIgualat) {
+          console.log(
+            `Ets un crack! Has igualat el set i mig, apostant ${aposta}€.`
+          );
+        } else {
+          console.log(
+            `T'has plantat a ${jugador.punts} punts, apostant ${aposta}€.`
           );
         }
+        // Ara juga la banca.
+        console.log(`\nAra juga la banca.`);
+
+        banca.punts = 0;
+        esSimSuperat = false;
+        do {
+          carta = baralla.shift();
+          banca.punts += carta.valorSIM;
+          console.log(
+            `  ${carta.numCarta} ${carta.preposPal}${carta.palCarta}. Punts: ${banca.punts}`
+          );
+          if (banca.punts > 7.5) {
+            esSimSuperat = true;
+          } else if (banca.punts >= jugador.punts) {
+            guanyaLaBanca = true;
+          }
+        } while (!esSimSuperat && !guanyaLaBanca);
+        if (guanyaLaBanca) {
+          console.log(`La banca s'ha plantat a ${banca.punts} punts.`);
+        }
       }
-    } while (
-      esCanviAposta ||
-      (!esSimSuperat &&
-        !esSimIgualat &&
-        prompt(`Et vols plantar (Si:s No:Intro)? `) != "s")
-    );
-    if (esSimSuperat) {
-      console.log(`Has superat el límit!`);
-      guanyaLaBanca = true;
-    } else {
-      if (esSimIgualat) {
+      if (guanyaLaBanca) {
+        banca.saldo += aposta;
+        jugador.saldo -= aposta;
         console.log(
-          `Ets un crack! Has igualat el set i mig, apostant ${aposta}€.`
+          `\nHa guanyat la banca. Has perdut ${aposta}€. El teu saldo és de ${jugador.saldo}€.`
         );
       } else {
+        banca.saldo -= aposta;
+        jugador.saldo += aposta;
         console.log(
-          `T'has plantat a ${puntsJugador} punts, apostant ${aposta}€.`
+          `\nHas guanyat la ronda. Has guanyat ${aposta}€. El teu saldo és de ${jugador.saldo}€.`
         );
       }
-      // Ara juga la banca.
-      console.log(`\nAra juga la banca.`);
-
-      esSimSuperat = false;
-      let puntsBanca = jugadors[0].punts;
-      do {
-        carta = barallaSIM.shift();
-        puntsBanca += carta.valorSIM;
-        console.log(
-          `  ${carta.numCarta} ${carta.preposPal}${carta.palCarta}. Punts: ${puntsBanca}`
-        );
-        if (puntsBanca > 7.5) {
-          esSimSuperat = true;
-        } else if (puntsBanca >= puntsJugador) {
-          guanyaLaBanca = true;
-        }
-      } while (!esSimSuperat && !guanyaLaBanca);
-      if (guanyaLaBanca) {
-        console.log(`La banca s'ha plantat a ${puntsBanca} punts.`);
-      }
-    }
-    if (guanyaLaBanca) {
-      jugadors[0].saldo += aposta;
-      jugadors[i].saldo -= aposta;
-      console.log(
-        `\nHa guanyat la banca. Has perdut ${aposta}€. El teu saldo és de ${jugadors[i].saldo}€.`
-      );
     } else {
-      jugadors[0].saldo -= aposta;
-      jugadors[i].saldo += aposta;
-      console.log(
-        `\nHas guanyat la ronda. Has guanyat ${aposta}€. El teu saldo és de ${jugadors[i].saldo}€.`
-      );
+      console.log(`\n${jugador.nom}, no tens saldo; no pots seguir jugant.`);
     }
-  }
+  };
 }
 
 // Mostra el resultat de la partida.
 console.log(`\nResulat final:`);
-for (let i = 0; i < jugadors.length; i++) {
-  console.log(`  ${jugadors[i].nom}: ${jugadors[i].saldo}€.`);
-}
+jugadors.forEach(jugador => {
+  console.log(`  ${jugador.nom}: ${jugador.saldo}€.`);
+});
